f7e34ac94dbef1f086b2b6ea4c145427e5474759
br1an17 <brianzairpaez@gmail.com>
2026-06-02 22:20:17 -0300

    escalon 3 terminado

--- ejerciciosDePOO/escalon3POO.js
+++ ejerciciosDePOO/escalon3POO.js
@@ -6,3 +6,34 @@
 // Tiene el método calcularTotalHoteles()
 //  que usa .reduce() para sumar y devolver la propiedad precioPorNoche
 //  de todos los hoteles de la lista.
+
+import Hotel from "./escalon1POO.js";
+
+class Agencia {
+  constructor() {
+    this.listaHoteles = [];
+  }
+  agregarHotel(hotel) {
+    this.listaHoteles.push(hotel);
+  }
+  calcularTotalHoteles() {
+ return this.listaHoteles.reduce(
+      (acumulador, hotel) => acumulador + hotel.precioPorNoche, 0
+    );
+    
+  }
+}
+export default Agencia;
+
+// --- DATOS DE PRUEBA (Asegurate de tener importado HotelVip si estás en otro archivo) ---
+const miAgencia = new Agencia();
+
+// Usamos objetos literales simples para probar rápido que la cuenta funcione:
+const h1 = { nombre: "Hotel Económico", precioPorNoche: 2000 };
+const h2 = { nombre: "Hotel Premium", precioPorNoche: 8000 };
+
+miAgencia.agregarHotel(h1);
+miAgencia.agregarHotel(h2);
+
+console.log("El total esperado es 10000:");
+console.log(miAgencia.calcularTotalHoteles());
